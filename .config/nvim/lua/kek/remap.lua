27b28e349a16ca9f616e7182985f52c61ab99bea
Kekkokers <kekkokers@gmail.com>
2023-04-05 22:09:52 +0300

    nvim changes

--- .config/nvim/lua/kek/remap.lua
+++ .config/nvim/lua/kek/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>w",  ':w<CR>')
+vim.keymap.set("n", "<leader>w", ':w<CR>')
+vim.keymap.set("n", "<leader>q", ':wq!<CR>')
 
 function _lazygit_toggle()
     local Terminal  = require('toggleterm.terminal').Terminal
@@ -9,3 +10,10 @@ function _lazygit_toggle()
 end
 
 vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  buffer = buffer,
+  callback = function()
+    vim.lsp.buf.format { async = false }
+  end
+})
